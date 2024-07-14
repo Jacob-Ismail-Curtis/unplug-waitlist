774d3c94728ae3832ad3dda477d9691aab19623f
jcgit786 <jacobcurtis786@gmail.com>
2024-07-14 11:25:15 +0100

    Commit 4

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
-/* eslint-disable import/no-extraneous-dependencies */
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { getFirestore } from 'firebase/firestore';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAZLG6MlHKD3NSZirVCjHc04haDWoS2ySQ',
-  authDomain: 'unplug-ec0c8.firebaseapp.com',
-  projectId: 'unplug-ec0c8',
-  storageBucket: 'unplug-ec0c8.appspot.com',
-  messagingSenderId: '972396344063',
-  appId: '1:972396344063:web:995af0cf67e4e5183092ca',
-  measurementId: 'G-DRSK5DF9DV',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
